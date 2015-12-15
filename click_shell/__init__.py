863b4688644714ba3ba8d1884327be102353e311
Clark Perkins <clark.perkins@digitalreasoning.com>
2015-12-14 22:01:32 -0600

    Updated docs

--- click_shell/__init__.py
+++ click_shell/__init__.py
@@ -4,11 +4,12 @@ click-shell
 An extension to click that easily turns your click app into a shell utility
 """
 
-from click_shell.core import make_click_shell
-from click_shell.decorators import shell
+from .core import make_click_shell, Shell
+from .decorators import shell
 
 
 __all__ = [
     'make_click_shell',
     'shell',
+    'Shell',
 ]
