dcb8451728aeb8db5aa190e71907af2e9e6372c4
Clark Perkins <clark.perkins@digitalreasoning.com>
2020-02-16 17:32:13 -0600

    Fixing reports

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p reports
+
 # Code linting
 pycodestyle click_shell
 pylint click_shell --reports=n --exit-zero --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" > reports/pylint.txt
@@ -8,8 +10,6 @@ pylint click_shell --reports=n --exit-zero --msg-template="{path}:{line}: [{msg_
 if [ "$TRAVIS_PYTHON_VERSION" != "2.7" ]; then
   mypy click_shell
 fi
-
-mkdir -p reports
 
 # Unit tests
 pytest --cov=click_shell --cov-report=xml
