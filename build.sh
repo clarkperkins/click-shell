bfc7412de1f93a92c0a04bc03c25f600a41484b3
Clark Perkins <clark.perkins@digitalreasoning.com>
2020-02-16 17:34:24 -0600

    Adding some logging

--- build.sh
+++ build.sh
@@ -3,11 +3,15 @@
 mkdir -p reports
 
 # Code linting
+echo "Running pycodestyle..."
 pycodestyle click_shell
+
+echo "Running pylint..."
 pylint click_shell --reports=n --exit-zero --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" > reports/pylint.txt
 
 # Type checking
 if [ "$TRAVIS_PYTHON_VERSION" != "2.7" ]; then
+  echo "Running mypy..."
   mypy click_shell
 fi
 
