2f689e6c056dd5a3acbc5f4245507cb3c44c932b
Clark Perkins <clark.perkins@trillianthealth.com>
2021-06-26 16:56:32 -0500

    Have travis run tests on multiple click versions

--- build.sh
+++ build.sh
@@ -12,7 +12,8 @@ echo "Running pylint..."
 pylint click_shell --reports=n --exit-zero --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" > reports/pylint.txt
 
 # Type checking
-if [ "$TRAVIS_PYTHON_VERSION" != "2.7" ]; then
+# mypy doesn't work at all on python2, and there's some other issues with pipenv packages on python3 < 3.8
+if [ "$TRAVIS_PYTHON_VERSION" == "3.9" ]; then
   echo "Running mypy..."
   mypy click_shell
 fi
@@ -20,7 +21,7 @@ fi
 # Unit tests
 pytest -vv --cov=click_shell --cov-report=xml
 
-if [ "$TRAVIS_PYTHON_VERSION" == "3.5" ] && [ "$TRAVIS_TAG" == "" ]; then
-  echo "Python 3.5 detected, running sonar-scanner"
+if [ "$TRAVIS_PYTHON_VERSION" == "3.9" ] && [ "$CLICK_VERSION" == "8.0" ] && [ "$TRAVIS_TAG" == "" ]; then
+  echo "Python 3.9 detected, running sonar-scanner"
   sonar-scanner -Dsonar.projectVersion=$(python setup.py --version)
 fi
