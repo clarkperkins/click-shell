12d4544e8475419c81f32b412c9eba04abe3dd73
Clark Perkins <clark.perkins@trillianthealth.com>
2021-06-27 19:52:58 -0500

    Allow for a custom subclass of ClickShell

--- click_shell/__init__.py
+++ click_shell/__init__.py
@@ -4,13 +4,14 @@ click-shell
 An extension to click that easily turns your click app into a shell utility
 """
 
-from .core import make_click_shell, Shell
+from .core import make_click_shell, Shell, ClickShell
 from .decorators import shell
 
 __all__ = [
     'make_click_shell',
     'shell',
     'Shell',
+    'ClickShell',
     '__version__',
 ]
 
